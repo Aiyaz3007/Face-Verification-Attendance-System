a981178e9bb3bd541601b07ab5dffd686bd169d3
Aiyaz3007 <maiyaz3007@gmail.com>
2024-02-26 21:49:46 +0530

    pipeline initial push

---
+++ main.py
@@ -0,0 +1,118 @@
+import os
+import cv2
+import numpy as np 
+import time
+
+from facenet_pytorch import InceptionResnetV1
+from retinaface import RetinaFace
+import torch.nn.functional as F  
+
+from tqdm import tqdm as loading_bar
+from datetime import date
+import sys
+
+import constants
+from utils import calculate_embedding,StudentDataHandler
+
+# Load the PyTorch FaceNet model
+model = InceptionResnetV1(pretrained='vggface2').eval().to(constants.device)
+
+# Load RetinaFace detector
+detector = RetinaFace(quality="normal")
+
+# creating object for student csv handler 
+student_db_handler = StudentDataHandler(json_file=constants.students_json_file,
+                                        csv_file=constants.attendance_file)
+
+students_reg_no = student_db_handler.get_all_regno()
+
+# Load known faces and their corresponding names
+known_faces = []
+known_names = []
+
+
+for file_path in loading_bar(student_db_handler.get_all_imagenames()):
+    try:
+        platform_specific_path = os.path.normpath(file_path)
+        filename_with_extension = os.path.basename(platform_specific_path)
+        name = os.path.splitext(os.path.basename(platform_specific_path))[0]
+
+        image = cv2.imread(os.path.join(constants.known_images_folder, filename_with_extension))
+        face_data = detector.predict(image)[0]
+
+        cropped_face = image[face_data["y1"]:face_data["y2"],face_data["x1"]:face_data["x2"]]
+        embedding = calculate_embedding(cropped_face,model)
+        known_faces.append(embedding)
+        known_names.append(name)
+
+    except Exception as e:
+        print(e)
+        print(f"error occured on image: {file_path}")
+        print("give some proper image") 
+        sys.exit()
+        
+
+
+# Initialize video capture
+video_capture = cv2.VideoCapture(constants.input_format)
+
+# Initialize variables for FPS calculation
+start_time = time.time()
+frame_count = 0
+
+while video_capture.isOpened():
+    ret,frame = video_capture.read()
+    try:
+        faces = detector.predict(frame)
+        for idx,face in enumerate(faces):
+            if all(key in face for key in ['x1', 'y1', 'x2', 'y2']):
+                x1, y1, x2, y2 = face['x1'], face['y1'], face['x2'], face['y2']
+                # Extract face region from the frame
+                cropped_face = frame[y1:y2, x1:x2]
+                # Preprocess and extract embedding for the detected face
+                detected_embedding = calculate_embedding(cropped_face,model)
+
+                similarities = [F.cosine_similarity(detected_embedding, known_embedding).item() for known_embedding in known_faces]
+                max_index = np.argmax(similarities)
+
+                if similarities[max_index] > constants.threshold:
+                    name = known_names[max_index]
+                    current_student_regno = students_reg_no[max_index]
+                    
+                    info = student_db_handler.get_info_from_reg_no(current_student_regno)
+
+                    print("-------------------")
+                    print(info[str(date.today())])
+                    print("-------------------")
+                    
+
+                    if info[str(date.today())] in ["P","A"]:
+                        if info[str(date.today())] == "A":
+                            student_db_handler.update_status_for_reg_no(current_student_regno,"P")
+                    else:
+                        student_db_handler.update_status_for_reg_no(current_student_regno,"A")
+                else:
+                    name = "unknown"
+                
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+
+                
+
+    except Exception as e:
+        print(e)
+
+    # Calculate FPS
+    frame_count += 1
+    elapsed_time = time.time() - start_time
+    fps = frame_count / elapsed_time
+
+    # Display FPS on the frame
+    cv2.putText(frame, f"FPS: {round(fps, 2)}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("frame",frame)
+    k = cv2.waitKey(1) 
+    if k == 27:
+        break
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ utils/csv_utils.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import json
+from datetime import date
+from os.path import exists
+
+class StudentDataHandler:
+    def __init__(self, json_file, csv_file):
+        self.json_file = json_file
+        self.csv_file = csv_file
+        self.json_data = self.load_json_data()
+        self.current_date = str(date.today())
+        
+        if exists(csv_file):
+            self.csv_data = self.load_csv_data()
+        else:
+            self.create_csv_file()
+
+    def get_all_regno(self):
+        return [self.json_data["students"][regno]["reg_no"] for regno in self.json_data["students"]]
+
+    def get_all_imagenames(self):
+        return [self.json_data["students"][regno]["image_path"] for regno in self.json_data["students"]]
+    
+    def load_json_data(self):
+        with open(self.json_file, 'r') as f:
+            return json.load(f)
+
+    def load_csv_data(self):
+        return pd.read_csv(self.csv_file)
+    
+    def create_csv_file(self):
+        # Create an empty DataFrame
+        # empty_df = pd.DataFrame(columns=['reg_no', 'name', self.current_date])
+        sample_data = {"reg_no": [],"name": [], self.current_date : []}
+        
+        for reg_no in self.json_data["students"]:
+            sample_data["reg_no"].append(reg_no)
+            sample_data["name"].append(self.json_data["students"][reg_no]["name"])
+            sample_data[self.current_date].append("")
+        
+        empty_df = pd.DataFrame(sample_data)
+        
+        # Save the empty DataFrame to a new CSV file
+        empty_df.to_csv(self.csv_file, index=False,)
+        # Load the newly created CSV file into self.csv_data
+        self.csv_data = self.load_csv_data()
+    
+    def get_info_from_reg_no(self, reg_no):
+        # Filter rows where 'reg_number' matches the given registration number
+        info = self.csv_data[self.csv_data['reg_no'] == reg_no]
+        
+        # Check if any row matches the registration number
+        if not info.empty:
+            return info.to_dict(orient='records')[0]
+        else:
+            return None
+        
+    def update_status_for_reg_no(self, reg_no, status):
+        # Check if the given registration number exists in the DataFrame
+        if reg_no in self.csv_data['reg_no'].values:
+            # Update the status for the corresponding registration number and current date column
+            self.csv_data.loc[self.csv_data['reg_no'] == reg_no, self.current_date] = status
+            # Save the updated DataFrame to the CSV file
+            self.csv_data.to_csv(self.csv_file, index=False)
+            print("Status updated successfully for registration number", reg_no)
+        else:
+            print("Registration number", reg_no, "not found.")
